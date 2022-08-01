aa0ad768375fce27dfd3d5d4c16fae481f2d27e8
Sajidur Rahman <sajibsrs@gmail.com>
2022-08-01 12:18:23 +0600

    added visitor pattern usage example

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
+use DesignPatterns\Behavioral\Visitor\HtmlNodeVisitor;
+use DesignPatterns\Behavioral\Visitor\ImageNode;
+use DesignPatterns\Behavioral\Visitor\TextNode;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo "Welcome!\n";
+/**
+ * Visitor pattern usage example
+ */
+$visitor    = new HtmlNodeVisitor();
+$imageNode  = new ImageNode("image.png");
+$textNode   = new TextNode("Hello text");
+
+echo $imageNode->accept($visitor);
+echo $textNode->accept($visitor);
