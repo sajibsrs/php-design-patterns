650e87f957f4b6bae264bd6b63996a34a3db7086
Sajidur Rahman <sajibsrs@gmail.com>
2022-08-02 11:55:43 +0600

    added pattern usage example

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-use DesignPatterns\Behavioral\Visitor\HtmlNodeVisitor;
-use DesignPatterns\Behavioral\Visitor\ImageNode;
+use DesignPatterns\Behavioral\Observer\Mobile;
+use DesignPatterns\Behavioral\Observer\Desktop;
+use DesignPatterns\Behavioral\Observer\Subject;
+use DesignPatterns\Behavioral\Strategy\Context;
+use DesignPatterns\Behavioral\Strategy\HttpGet;
+use DesignPatterns\Behavioral\Strategy\HttpPost;
 use DesignPatterns\Behavioral\Visitor\TextNode;
+use DesignPatterns\Behavioral\Visitor\ImageNode;
+use DesignPatterns\Behavioral\Visitor\HtmlNodeVisitor;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -13,5 +19,39 @@ $visitor    = new HtmlNodeVisitor();
 $imageNode  = new ImageNode("image.png");
 $textNode   = new TextNode("Hello text");
 
-echo $imageNode->accept($visitor);
-echo $textNode->accept($visitor);
+print("--- Visitor pattern ---\n");
+
+printf("Image source: %s\n", $imageNode->accept($visitor));
+printf("Text content: %s\n", $textNode->accept($visitor));
+
+/**
+ * Strategy pattern usage example
+ */
+$context = new Context();
+$HttpGetStrategy = new HttpGet("/home");
+$context->setStrategy($HttpGetStrategy);
+
+print("--- Strategy pattern ---\n");
+
+printf("HTTP method: %s\n", $context->execute());
+printf("Request URL: %s\n", $context->getStrategy()->getUrl());
+
+$HttpSetStrategy = new HttpPost("/contact");
+$context->setStrategy($HttpSetStrategy);
+
+printf("HTTP method: %s\n", $context->execute());
+printf("Request URL: %s\n", $context->getStrategy()->getUrl());
+
+/**
+ * Observer pattern usage example
+ */
+$subject    = new Subject();
+$mobile     = new Mobile();
+$desktop    = new Desktop();
+
+$subject->attach($mobile);
+$subject->attach($desktop);
+
+print("--- Observer pattern ---\n");
+
+$subject->notify();
